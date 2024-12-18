9479c923916d9681012ebb09e91b99b7423cbb05
GilbertoM0 <124477251+GilbertoM0@users.noreply.github.com>
2024-12-17 21:14:39 -0600

    Commit primero de semantico
    Commit primero de semantico

---
+++ compiladorf/src/main/java/com/mycompany/compiladorf/Compiladorf.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compiladorf;
+
+/**
+ *
+ * @author gmva2
+ */
+public class Compiladorf {
+    
+    
+    
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
